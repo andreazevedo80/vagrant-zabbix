94889ed046bcee35608e2e2d1d49c1abad9a41d2
andreazevedo80 <andre.l.azevedo69@gmail.com>
2024-09-27 14:10:46 -0300

    Vagrant e Script Validados

--- scripts/install_zabbix.sh
+++ scripts/install_zabbix.sh
@@ -1,40 +1,57 @@
+# [Passos de instalação de acordo com fabricante para a distribuição selecionada Ubuntu 22.04 (Jammy)] Fonte: https://www.zabbix.com/download?zabbix=6.4&os_distribution=ubuntu&os_version=22.04&components=server_frontend_agent&db=mysql&ws=apache
+
 #!/bin/bash
 
 # Variáveis
-MYSQL_ROOT_PASSWORD="zabbix"
+MYSQL_ROOT_PASSWORD="mysqlpwd"
 ZABBIX_DB="zabbix"
 ZABBIX_USER="zabbix"
-ZABBIX_PASSWORD="zabbix"
+ZABBIX_PASSWORD="zabbixpwd"
 
 # Atualizar pacotes
 sudo apt-get update -y && sudo apt-get upgrade -y
 
-# Instalar o MySQL Server
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_ROOT_PASSWORD"
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD"
+# Instalar o MySQL
 sudo apt-get install -y mysql-server
 
-# Criar banco de dados e usuário Zabbix
-sudo mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE $ZABBIX_DB CHARACTER SET utf8 COLLATE utf8_bin;"
-sudo mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "CREATE USER '$ZABBIX_USER'@'localhost' IDENTIFIED BY '$ZABBIX_PASSWORD';"
-sudo mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON $ZABBIX_DB.* TO '$ZABBIX_USER'@'localhost';"
-sudo mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+# Iniciar e habilitar o MySQL
+sudo systemctl start mysql
+sudo systemctl enable mysql
 
-# Instalar dependências e baixar repositório do Zabbix
-sudo apt-get install -y wget lsb-release
-wget https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_$(lsb_release -cs)_all.deb
-sudo dpkg -i zabbix-release_$(lsb_release -cs)_all.deb
+# Configurar a senha root do MySQL e reforçar a segurança inicial
+sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$MYSQL_ROOT_PASSWORD';"
+sudo mysql -e "DELETE FROM mysql.user WHERE User='';"  # Remover usuários anônimos
+sudo mysql -e "DROP DATABASE IF EXISTS test;"          # Remover banco de dados de teste
+sudo mysql -e "FLUSH PRIVILEGES;"                      # Atualizar as permissões
+
+# Configurar o banco de dados do Zabbix
+mysql -uroot -p$MYSQL_ROOT_PASSWORD <<EOF
+CREATE DATABASE $ZABBIX_DB character set utf8mb4 collate utf8mb4_bin;
+CREATE USER '$ZABBIX_USER'@'localhost' IDENTIFIED BY '$ZABBIX_PASSWORD';
+GRANT ALL PRIVILEGES ON $ZABBIX_DB.* TO '$ZABBIX_USER'@'localhost';
+SET GLOBAL log_bin_trust_function_creators = 1;
+EOF
+
+# Baixar o repositório do Zabbix
+wget https://repo.zabbix.com/zabbix/6.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.4-1+ubuntu22.04_all.deb
+
+# Instalar o repositório
+sudo dpkg -i zabbix-release_6.4-1+ubuntu22.04_all.deb
+
+# Atualizar a lista de pacotes
 sudo apt-get update
 
-# Instalar Zabbix Server com MySQL e frontend
-sudo apt-get install -y zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-agent
+# Instalar o Zabbix server, frontend, e agent
+sudo apt-get install -y zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent
 
-# Importar o esquema inicial do banco de dados para o Zabbix
-sudo zcat /usr/share/doc/zabbix-server-mysql*/create.sql.gz | mysql -u$ZABBIX_USER -p$ZABBIX_PASSWORD $ZABBIX_DB
+# Importar o esquema inicial do Zabbix
+zcat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | mysql --default-character-set=utf8mb4 -u$ZABBIX_USER -p$ZABBIX_PASSWORD $ZABBIX_DB
 
-# Configurar o Zabbix Server para usar o banco de dados MySQL
-sudo sed -i "s/# DBPassword=/DBPassword=$ZABBIX_PASSWORD/" /etc/zabbix/zabbix_server.conf
+# Configurar o Zabbix server
+sed -i "s/# DBPassword=/DBPassword=$ZABBIX_PASSWORD/" /etc/zabbix/zabbix_server.conf
 
-# Reiniciar e habilitar os serviços do Zabbix e do Apache
-sudo systemctl restart zabbix-server zabbix-agent apache2
-sudo systemctl enable zabbix-server zabbix-agent apache2
+# Reiniciar e habilitar serviços
+systemctl restart zabbix-server zabbix-agent apache2
+systemctl enable zabbix-server zabbix-agent apache2
+
+
